26fd1f6a8523951770897e7602ab210898d0b2f9
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-07-06 15:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+  
+    bool isPossible(int mid, vector<int>& arr, int k){
+        int cows = 1;
+        int prev = 0;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-arr[prev] >=mid){
+                cows++;
+                prev=i;
+            }
+        }
+        return cows >= k;
+    }
+    int aggressiveCows(vector<int> &arr, int k) {
+        // code here
+        sort(arr.begin(),arr.end());
+        int low=1;
+        int high = arr[arr.size()-1]-arr[0];
+        int ans;
+        while(low<=high){
+            int mid = (low+high)/2;
+            // function to check
+            if(isPossible(mid,arr,k)==true){
+                ans= mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
